f32a92e9d480ef65165d7b7ff794b8958fe28802
wannabecunny <111440106+wannabecunny@users.noreply.github.com>
2025-07-21 20:21:53 +0800

    N/A instead of 0 in location
    N/A instead of 0 in location

--- db.sql
+++ db.sql
@@ -44,7 +44,7 @@ CREATE TABLE `items` (
   `description` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `location` VARCHAR(255) DEFAULT NULL,
+  `location` VARCHAR(255) DEFAULT 'N/A',
   PRIMARY KEY (`id`),
   KEY `category_id` (`category_id`),
   CONSTRAINT `items_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
